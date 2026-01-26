614fb5e067a68db5a1abdb0e662a015bad0a8873
hojun <hoj0902@naver.com>
2026-01-26 09:49:08 +0900

    26.01.26 08_01_data_structure commit

---
+++ 08_01_data_structure/02_sequence_method.py
@@ -0,0 +1,14 @@
+# index
+text = 'banana'
+print(None)  # 1
+
+my_list = [1, 2, 3]
+print(None)  # 1
+
+
+# count
+text = 'banana'
+print(None)  # 3
+
+my_list = [1, 2, 2, 3, 3, 3]
+print(None)  # 3
